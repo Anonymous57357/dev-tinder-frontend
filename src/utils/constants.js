310d999e63ff874224d4e67fce6b59ecef82a396
mukeshvijayakumar <mukesh@techasoft.com>
2025-01-22 11:41:02 +0530

    changes

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,8 +1,8 @@
 // LOCAL HOST
-export const BASE_URL = 'http://localhost:8000';
+// export const BASE_URL = 'http://localhost:8000';
 
 // RENDER
-// export const BASE_URL = 'https://dev-tinder-h75u.onrender.com';
+export const BASE_URL = 'https://dev-tinder-h75u.onrender.com';
 
 export const FACEBOOK_URL = '';
 
