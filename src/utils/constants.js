ad16fd95bad42cc2f6473b9ee35a6f8a7f6737da
mukeshvijayakumar <mukesh@techasoft.com>
2025-01-27 18:16:25 +0530

    updated BASEURL

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,11 +1,6 @@
 // LOCAL HOST
-export const BASE_URL = 'http://localhost:8000';
+// export const BASE_URL = 'http://localhost:8000';
 
-// RENDER
-// export const BASE_URL = 'https://dev-tinder-h75u.onrender.com';
+export const BASE_URL = '/api';
 
-export const FACEBOOK_URL = '';
-
-export const TWITTER_URL = '';
-
-export const LINKEDIN_URL = '';
+// api is nothing but a local host using aws instance deployed nginx server http://localhost:8000
