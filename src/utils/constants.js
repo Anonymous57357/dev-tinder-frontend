257bfeef9cc5c2eb72719cd8f187b50705df4cbf
mukeshvijayakumar <mukesh@techasoft.com>
2025-01-28 12:59:06 +0530

    changed-navigation && dynamic url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,2 @@
-// LOCAL HOST
-// export const BASE_URL = 'http://localhost:8000';
-
-export const BASE_URL = '/api';
-
-// api is nothing but a local host using aws instance deployed nginx server http://localhost:8000
+export const BASE_URL =
+  location.hostname === 'localhost' ? 'http://localhost:8000' : '/api';
